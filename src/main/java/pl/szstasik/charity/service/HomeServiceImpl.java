8c162a688296830f9e1fddc68bb731b643450fe7
Szymonidas <sz.stasik@gmail.com>
2022-01-17 11:09:21 +0100

    sum of all bags

--- src/main/java/pl/szstasik/charity/service/HomeServiceImpl.java
+++ src/main/java/pl/szstasik/charity/service/HomeServiceImpl.java
@@ -1,6 +1,7 @@
 package pl.szstasik.charity.service;
 
 import org.springframework.stereotype.Service;
+import pl.szstasik.charity.model.Donation;
 import pl.szstasik.charity.model.Institution;
 import pl.szstasik.charity.repository.DonationRepository;
 import pl.szstasik.charity.repository.InstitutionRepository;
@@ -21,7 +22,12 @@ public class HomeServiceImpl implements HomeService{
 
     @Override
     public int bagsSum() {
-        return 0;
+        List<Donation> allDonations = donationRepository.findAll();
+        int allBags = 0;
+        for (Donation donation : allDonations) {
+            allBags += donation.getQuantity();
+        }
+        return allBags;
     }
 
     @Override
